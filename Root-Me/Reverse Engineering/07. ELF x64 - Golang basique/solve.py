49bace13899e5ba6732670638f317447d786114a
tjoloi <36538146+tjoloi@users.noreply.github.com>
2019-11-27 16:37:00 -0500

    Reversed Go basic + fixed xor tool

--- Root-Me/Reverse Engineering/07. ELF x64 - Golang basique/solve.py
+++ Root-Me/Reverse Engineering/07. ELF x64 - Golang basique/solve.py
@@ -7,3 +7,10 @@ def get_encrypting_key(key, data_length):
 		encrypting_key += key
 	encrypting_key = encrypting_key[:data_length]
 	return encrypting_key
+
+key = b'rootme'
+passwd = b'\x3B\x02\x23\x1B\x1B\x0C\x1C\x08\x28\x1B\x21\x04\x1C\x0B'
+
+real_key = get_encrypting_key(key, len(passwd))
+
+print(xor_everything(passwd, real_key))
